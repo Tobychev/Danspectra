ecc4f70e8f2615a9d608c6c6541e748836f955ac
Tomas Bylund <tomas.bylund@gmail.com>
2016-01-18 16:09:20 +0100

    Initial commit, some work in progress.

---
+++ initial.py
@@ -0,0 +1,20 @@
+import os
+import numpy as np
+import shutil
+
+
+from pyraf import iraf
+
+iraf.images(_doprint=0)
+iraf.imutil(_doprint=0)
+iraf.noao(_doprint=0)
+iraf.imred(_doprint=0)
+iraf.ccdred(_doprint=0)
+iraf.twodspec(_doprint=0)
+iraf.longslit(_doprint=0)
+iraf.kpnoslit(_doprint=0)
+iraf.astutil(_doprint=0)
+iraf.onedspec(_doprint=0)
+iraf.images(_doprint=0)
+iraf.imutil(_doprint=0)
+
