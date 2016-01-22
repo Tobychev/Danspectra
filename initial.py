b3a8b91b26d1d36400e91d4f88559128ea83570c
Tomas Bylund <tomas.bylund@gmail.com>
2016-01-22 16:56:07 +0100

    Added newly created continium module, some other additions

--- initial.py
+++ initial.py
@@ -1,6 +1,10 @@
 import matplotlib.pyplot as pl
+import kontin as con
 import numpy as np
 import danspec as d
+
+reload(d)
+reload(con)
 
 def find_nearest(array,value):
     return (np.abs(array-value)).argmin()
@@ -13,4 +17,6 @@ fil = "6405_aS1_388_cor.fits"
 spec = d.danspectra(fil)
 
 
+fil2 = "6405_aS1_397_cor.fits"
+spec2 = d.danspectra(fil2)
 
