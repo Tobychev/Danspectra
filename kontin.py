e8feba0e918b44bbf7a97e4dd0f8a53c0d3a2041
Tomas Bylund <tomas.bylund@gmail.com>
2016-01-26 11:54:26 +0100

    Made function for turning index arrays into window definitions, refactored out setting of window definitions from manual window selection function.

--- kontin.py
+++ kontin.py
@@ -8,19 +8,40 @@ def make_idx_from_windows(windows):
         idbg = idbg + range(win[0],win[1]+1)
     return idbg
 
-def select_background_automatic(danspec,metod,line="mean"):
+def make_windows_from_idx(idx):
+    window  = []
+    windows = []
+ 
+    window.append(idx[0])
+    # The subtraction allows for a cleaner
+    # condition in the loop, by preventing an
+    # out-of-bounds attempt. The add in the last
+    # edge by hand.
+    for i in range(1,len(idx)-1):
+        if idx[i]+1 != idx[i+1]:
+            print idx[i]+1, idx[i+1]
+            window.append(idx[i])
+            windows.append(window)
+            window = [idx[i+1]]
+    window.append(idx[-1])
+    windows.append(window)
+    return windows
+
+def select_bgwin_auto(danspec,metod,line="mean"):
     if line == "mean":
         data = danspec.mean
     else:
         data = danspec.spec(line)
 
-    if metod == "tophundred":
+    if   metod == "over1":
+        return np.flatnonzero(data > 1)
+    elif metod == "tophundred":
         return top_number(data,100)
-    if metod == "top 5%":
+    elif metod == "top 5%":
         return top_number(data,round(len(data)*0.05))
-    if metod == "top 10%":
+    elif metod == "top 10%":
         return top_number(data,round(len(data)*0.1))
-    if metod == "90-95 decile":
+    elif metod == "90-95 decile":
         t5  = top_number(data,round(len(data)*0.05))[-1]
         t10 = top_number(data,round(len(data)*0.1))
         return t10[ t10 >= t5 ]
@@ -32,14 +53,9 @@ def top_number(data,number):
     
 window  = []
 windows = []
-def select_background_manual(danspec):
+def select_bgwin_manual(danspec):
         print "Selecting background windows"
 
-        if len(danspec.bgwindows) > 0:
-            print "WARNING: background windows already defined"
-            print "WARNING: all old values will be erased"
-            if not raw_input("Continue Y/N? ").lower() == "y":
-                return None
         windows = []
         fig = pl.figure()
         ax  = fig.add_subplot(111)
@@ -50,8 +66,6 @@ def select_background_manual(danspec):
         cursor = Cursor(ax,useblit=True, color='red', 
                         linewidth=1,horizOn=False)
         pl.show()
-        if len(windows) > 0:
-            danspec.set_bgwindows(windows,warn=False) #Already asked
 
         return make_idx_from_windows(windows)
 
