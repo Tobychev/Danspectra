d16abb1c1f4c23fd37ce6c79f9e215a0e48c22f5
Tomas Bylund <tomas.bylund@gmail.com>
2016-02-10 17:59:48 +0100

    Added facility to ignore all values below a certain decile (useful for getting rid of lines).

--- visualize.py
+++ visualize.py
@@ -1,3 +1,4 @@
+import numpy as np
 import matplotlib.pyplot as pl
 
 def show_fit_with_points(spec,idx,fit,line="mean"):
@@ -15,16 +16,21 @@ def show_fit_on_curve(fit,xs,curve):
     pl.plot(xs,curve,'b')
     pl.show()
 
-def plot_fits_stats(data,ref,names,title="",bins=43,cols=2,yscale="linear"):
+def plot_fits_stats(data,ref,names,title="",bins=43,cols=2,yscale="linear",cutoff=0):
     fig = pl.figure()
     pl.suptitle(title,fontsize=14)
     rows = len(names)/cols
     if len(names)%cols > 0:
         rows+=1
 
+
     for i,key in enumerate(names):
+        if cutoff > 0:
+            idx = np.where(data[key] > np.percentile(data[key],q=cutoff))
+        else:
+            idx = range(0,len(data[key]))
         ax = fig.add_subplot(rows,cols,i+1)
-        ax.hist(data[key],bins=bins)
+        ax.hist(data[key][idx],bins=bins)
         ax.axvline(ref[key],linestyle="dashed",color="r")
         ax.set_yscale(yscale)
         ax.set_title(key)
