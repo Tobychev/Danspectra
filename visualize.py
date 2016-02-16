e991ede697beed9f534c639513c14f66d7d9c135
Tomas Bylund <tomas.bylund@gmail.com>
2016-02-16 16:18:29 +0100

    Some functions for plotting background windows, moved interactive window selection into its own file

--- visualize.py
+++ visualize.py
@@ -1,5 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as pl
+import matplotlib.cm as cm
+
+def get_colours(num,colour_name="Oranges"):
+    return iter(cm.__dict__[colour_name](np.linspace(0,1,num)))
 
 def show_linegroup_on_curve(lines,xs,ys):
     pl.plot(xs,ys)
@@ -29,6 +33,14 @@ def show_spec_lines(spec,lines):
 
     pl.show()
 
+def show_dict_wins_on_curve(wins,xs,ys):
+    colour = get_colours(len(wins))
+    pl.plot(xs,ys)
+    for i,name in enumerate(wins):
+        idx = wins[name]
+        pl.plot(xs[idx],ys[idx],'o',c=next(colour))
+
+    pl.show()
 
 def show_fit_with_points(spec,idx,fit,line="mean"):
     if line == "mean":
@@ -65,42 +77,4 @@ def plot_fits_stats(data,ref,names,title="",bins=43,cols=2,yscale="linear",cutof
         
     pl.tight_layout()
     pl.show()
-
-window  = []
-windows = []
-def select_manual_windows(danspec):
-        global windows
-        print "Selecting windows"
-
-        windows = []
-        fig = pl.figure()
-        ax  = fig.add_subplot(111)
-        ax.plot(danspec.mean)
-        ax.plot([0,len(danspec.mean)],[1,1],'r')
-        axis = wdg.AxesWidget(ax)
-        axis.connect_event("key_press_event",__manageux)
-        cursor = wdg.Cursor(ax,useblit=True, color='red', 
-                        linewidth=1,horizOn=False)
-        pl.show()
-        return windows
-
-def __manageux(event):
-    global window, windows
-    if event.key == "?":
-        print """ 
-a - add point to window, after two points have been indicated a new window is defined
-"""            
-    elif event.key == "a":
-        pos = int(round(event.xdata))
-        print "adding ", pos
-        if len(window) == 0:
-            window.append(pos)
-        elif len(window) == 1:
-            if window[0] < event.xdata:
-                window.append(pos)
-            else:
-                window.append(window[0])
-                window[0] = pos
-            windows.append(window)
-            window=[]
 
