ab81aa5471c3a8165532d52f99b5ac955a9c0305
Tomas Bylund <tomas.bylund@gmail.com>
2016-02-16 16:19:52 +0100

    Work on gettig line processing to work

--- initial.py
+++ initial.py
@@ -4,15 +4,17 @@ import numpy as np
 import danspec as d
 import testwin as tw
 import visualize as vis
-
+import lines as lin
+import interactive as intr
 reload(d)
 reload(tw)
 reload(con)
+reload(lin)
+reload(vis)
+reload(intr)
 
 def find_nearest(array,value):
     return (np.abs(array-value)).argmin()
-
-
 
 # rows     - spectra
 # columns  - dispersion/space axis
@@ -22,9 +24,7 @@ spec2 = d.danspectra(fil2)
 wn = tw.gen_all_auto_wins(spec2)
 wn = tw.gen_man_win(spec2,wn)
 
-#refmean,refstd = tw.compare_win_continua(spec2,wn,centre="median",plot=False,bins=44)
-#refmean,refstd = tw.compare_win_continua(spec2,wn,centre="smoothmax",plot=False)
-#menmean,menstd = tw.compare_win_continua(spec2,wn,centre="mean",plot=False)
+lines = lin.make_lines_from_wins(spec2,spec2.pkwindows)
 
-result = tw.test_fit_with_noise(spec2,rows=1e4,plot_excess=True,bins=103,cut=25)
-
+lines = lines + lin.subdivide_line(lines[0])
+lines = lines[1:]
