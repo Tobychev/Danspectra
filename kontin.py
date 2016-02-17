b4491485d1695a34de0a9eceb751dbe7bc275b9a
Tomas Bylund <tomas.bylund@gmail.com>
2016-02-17 16:12:29 +0100

    Created continua class, refactored around a bit

--- kontin.py
+++ kontin.py
@@ -1,21 +1,21 @@
 import matplotlib.pyplot as pl
 import numpy as np
 
-def frame_fit(spec,idx):
-   return np.polyfit(spec.lmbd[idx],spec.data.transpose()[idx,:],1)
+def frame_fit(frame,idx):
+   return np.polyfit(frame.lmbd[idx],frame.data.transpose()[idx,:],1)
 
-def frame_subtract_cont(spec,idx):
-    fits = frame_fit(spec,idx)
-    cont = fits[1,:].reshape(len(fits[1,:]),1) + np.outer(fits[0,:],spec.lmbd)
-    return spec.data - cont
+def frame_subtract_cont(frame,idx):
+    fits = frame_fit(frame,idx)
+    cont = fits[1,:].reshape(len(fits[1,:]),1) + np.outer(fits[0,:],frame.lmbd)
+    return frame.data - cont
     
-def row_subtract_cont(spec,line,idx):
-    b,a = row_fit_continuum(spec,line,idx)
-    cont = a + b*spec.lmbd
-    return spec.spec(line)-cont 
+def row_subtract_cont(frame,line,idx):
+    b,a = row_fit_continuum(frame,line,idx)
+    cont = a + b*frame.lmbd
+    return frame.spec(line)-cont 
 
-def row_fit_continuum(spec,line,idx):
-    fit = np.polyfit(spec.lmbd[idx],spec.spec(line)[idx],1)
+def row_fit_continuum(frame,line,idx):
+    fit = np.polyfit(frame.lmbd[idx],frame.frame(line)[idx],1)
     return fit
 
 def make_idx_from_windows(windows):
@@ -43,14 +43,14 @@ def make_windows_from_idx(idx):
     windows.append(window)
     return windows
 
-def select_bgwin_auto(danspec,metod,row="mean",
+def select_bgwin_auto(danframe,metod,row="mean",
                         npoint=100,q=50):
     if row == "mean":
-        data = danspec.mean
+        data = danframe.mean
     elif row == "ref":
-        data = danspec.ref
+        data = danframe.ref
     else:
-        data = danspec.spec(row)
+        data = danframe.frame(row)
 
     if   metod == "over 1":
         return np.flatnonzero(data > 1)
@@ -65,7 +65,7 @@ def select_bgwin_auto(danspec,metod,row="mean",
         t10 = top_number(data,round(len(data)*0.1))
         return t10[ t10 >= t5 ]
     elif metod == "ref top":       
-        return range( danspec.ref.argmax()- 5, danspec.ref.argmax()+ 5 )
+        return range( danframe.ref.argmax()- 5, danframe.ref.argmax()+ 5 )
     elif metod == "segments":
         return top_of_segments(data,npoint,q)
 
@@ -87,7 +87,67 @@ def top_number(data,number):
     idx.sort()
     return idx
 
-def save_bgwin_from_idx(danspec,idx):
+def save_bgwin_from_idx(danframe,idx):
     wins = make_windows_from_idx(idx)
-    danspec.set_bgwindows(wins)
+    danframe.set_bgwindows(wins)
+
+
+class continua(object):
+    def __init__(self,danframe,extras=[]):
+        self.frame = danframe
+        self.top20 = self.auto_fit_frame("top 20")
+        self.top5p = self.auto_fit_frame("top 5%")
+        self.segmt = selt.auto_fit_frame("segments")
+        if len(extras) > 0:
+            self.extra = {}
+            for method in extras:           
+                self.extra[method] = self.auto_fit_frame(method)
+
+    def top20cont(self,wavlen):
+        return self.top20["k"]*wavlen + self.top20["m"]
+
+    def top5pcont(self,wavlen):
+        return self.top5p["k"]*wavlen + self.top5p["m"]
+
+    def segmtcont(self,wavlen):
+        return self.segmt["k"]*wavlen + self.segmt["m"]
+
+    def __top_number(self,data,number):
+        idx = np.argpartition(data,-number)[-number:]
+        idx.sort()
+        return idx
+
+    def __top_of_segments(self,data,npoint,q):
+        ids = (data > np.percentile(data,q)).nonzero()[0]
+        nregion = len(ids)/npoint
+        perreg  = npoint/nregion
+        regions = np.array_split(ids,nregion)
+        idx = np.array( [])
+        # Top perreg of data in each region
+        # have global indices given by reg, 
+        # top_number returns indiced local to data[reg]
+        for reg in regions:
+             idx = np.hstack( (idx, reg[top_number(data[reg],perreg)]) )
+    return idx.astype("int")
+
+    def auto_fit_frame(self,method):
+        idx = self.auto_select_bgwin(method)
+        k,m = np.polyfit(self.frame.lmbd[idx],self.frame.data.transpose()[idx,:],1)
+        return {"k":k,"m":m}
+            
+    def auto_select_bgwin(self,method, npoint=100,q=50):
+        data = self.frame.ref
+
+        if   metod == "top 100":
+            return top_number(data,100)
+        elif metod == "top 5%":
+            return top_number(data,round(len(data)*0.05))
+        elif metod == "top 20":
+            return top_number(data,round(len(data)*0.1))
+        elif metod == "90-95 decile":
+            t5  = top_number(data,round(len(data)*0.05))[-1]
+            t10 = top_number(data,round(len(data)*0.1))
+            return t10[ t10 >= t5 ]
+        elif metod == "segments":
+            return top_of_segments(data,npoint,q)
 
