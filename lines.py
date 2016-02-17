0d806a5bde792d383828c44092296f42627c196f
Tomas Bylund <tomas.bylund@gmail.com>
2016-02-17 16:15:11 +0100

    Updated line class to contain more functionality

--- lines.py
+++ lines.py
@@ -17,40 +17,6 @@ def make_lines_from_wins(danspec,wins):
 
     return lines
 
-def subdivide_line(line):
-    mins =find_minima(line.ref)
-    if len(mins) > 1:
-        lines = []    
-        mins  = intr.manual_delete_minima(mins,line)
-        for li in mins:
-            lines.append(dan.line( line.danspec, (line.idx[ li[0]] ,line.idx[li[2]]) ))
-
-        return lines
-
-    else:
-        return line
-
-def find_minima(curve):
-    deriv = np.diff(curve)
-    infl  = find_inflections(deriv)
-    mins  = []
-    for i,point in enumerate(infl):
-        if is_minima(deriv,point):                      
-            if i!= 0 and i < len(infl)-1:
-                mins.append( (infl[i-1],point, infl[i+1]) )
-
-    return mins
-
-def find_inflections(deriv):
-    mid =  np.where(np.diff(np.sign(deriv)))[0]+1
-    return np.concatenate( ([0],mid,[len(deriv)]) )
-    
-def is_minima(curve,point):
-    if curve[point-3:point].mean() < 0 and curve[point:point+3].mean() > 0:
-        return True
-    else:
-        return False
-
 def fit_linecores(line,xs,ys):
     guess = line.ref.argmin()
     bottom = line.idx[guess-4:guess+5]
@@ -67,3 +33,62 @@ def get_linecore(a,b,c):
     lam_min = -b/(2*a)
     lin_bot = np.polyval((a,b,c),lam_min)
     return lam_min,lin_bot
+
+class line(object):
+    def __init__(self,spec,winbounds):
+        self.idx  = self.__trim_line_indices(winbounds,spec)
+        self.win  = (self.idx[0], self.idx[-1])       
+        self.lmbd = spec.lmbd[self.idx]
+        self.ref  = spec.ref[self.idx]
+        self.name = str(self.lmbd.mean())
+        self.danspec = spec
+
+    def __repr__(self):
+        return "Line {} [{} to {}]".format(self.name,self.idx[0],self.idx[-1])
+    
+    def __trim_line_indices(self,winbounds,spec):
+        # Trims out values above one
+        idx = range(winbounds[0],winbounds[1]+1)
+        tmp = np.where(spec.ref[idx] > 1)[0]
+        cut = np.where(np.diff(tmp) > 1)[0] 
+        if len(tmp) <  2:
+            return idx
+        elif len(tmp) == 2 and len(cut) == 1:
+            return idx
+        elif len(cut) > 1:
+            return idx[tmp[cut]:tmp[cut+2]]
+        elif tmp[0] == 0:
+            return idx[tmp[-1]:]
+        else:
+            return idx[:tmp[1]]
+
+    def subdivide_line(self, line):
+        mins = self.__find_minima(line.ref)
+        if len(mins) > 1:
+            lines = []    
+            mins  = intr.manual_delete_minima(mins,line)
+            for li in mins:
+                lines.append(dan.line( line.danspec, (line.idx[ li[0]] ,line.idx[li[2]]) ))
+
+            return lines
+
+        else:
+            return line
+
+    def __find_minima(self,curve):
+        deriv = np.diff(curve)
+        mid   = np.where(np.diff(np.sign(deriv)))[0]+1
+        infl  = np.concatenate( ([0],mid,[len(deriv)]) )
+        mins  = []
+        for i,point in enumerate(infl):
+            if self.__is_minima(deriv,point):                      
+                if i!= 0 and i < len(infl)-1:
+                    mins.append( (infl[i-1],point, infl[i+1]) )
+
+        return mins
+        
+    def __is_minima(self,curve,point):
+        if curve[point-3:point].mean() < 0 and curve[point:point+3].mean() > 0:
+            return True
+        else:
+            return False
