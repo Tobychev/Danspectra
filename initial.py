ef6cffd1ac18ef8e793a4ca393abdb526f374976
Tomas Bylund <tomas.bylund@gmail.com>
2016-02-18 06:03:46 +0100

    Working version of setting up for fitting line cores

--- initial.py
+++ initial.py
@@ -1,12 +1,12 @@
 import matplotlib.pyplot as pl
 import kontin as con
 import numpy as np
-import danspec as d
+import danframe as dan
 import testwin as tw
 import visualize as vis
 import lines as lin
 import interactive as intr
-reload(d)
+reload(dan)
 reload(tw)
 reload(con)
 reload(lin)
@@ -18,13 +18,23 @@ def find_nearest(array,value):
 
 # rows     - spectra
 # columns  - dispersion/space axis
-fil2 = "local_data/6405_aS1_397_cor.fits"
-spec2 = d.danspectra(fil2)
+#wn = tw.gen_all_auto_wins(spec2)
+#wn = tw.gen_man_win(spec2,wn)
 
-wn = tw.gen_all_auto_wins(spec2)
-wn = tw.gen_man_win(spec2,wn)
 
-lines = lin.make_lines_from_wins(spec2,spec2.pkwindows)
+fil1 = "local_data/6405_aS1_397_cor.fits"
+spec1 = dan.danframe_sac(fil1)
+lines = lin.make_lines_from_wins(spec1,spec1.pkwindows)
 
-lines = lines + lin.subdivide_line(lines[0])
-lines = lines[1:]
+l1 = lines[2]
+
+#lin.fit_linecores(l1,spec2.lmbd,spec2.data)
+
+#lines = lines + lin.subdivide_line(lines[0])
+#lines = lines[1:]
+
+#con6405 = con.continua(spec2)
+
+fil = "local_data/6405_aS1"
+spec2 = dan.frameseries(fil,"top 5%")
+
