46398948ae3d6a2d568404ba3ada8f7e7129a1f9
Tomas Bylund <tomas.bylund@gmail.com>
2016-02-22 17:18:12 +0100

    A few notes

--- measure.py
+++ measure.py
@@ -1,4 +1,7 @@
+#encoding: utf8
 import matplotlib.pyplot as pl
+import interactive as intr
+import visualize as vis
 import danframe as dan
 import kontin as con
 import lines as lin
@@ -18,3 +21,15 @@ mes["FeI top 5%"]  = FeI.measure_linecores(s6405_t5p)
 mes["FeI segm"  ]  = FeI.measure_linecores(s6405_seg)
 mes["Myst top 5%"] = myst.measure_linecores(s6405_t5p)
 mes["Myst segm"  ] = myst.measure_linecores(s6405_seg)
+
+
+#Konstiga mätningar
+# Första bilden
+# Myst segm   : 10,406,693, (695)
+# Myst top 5% : 10,406,693, (695)
+# FeI  segm   : 313,314,315,316,317,799
+
+# 799 - helt klart skräp
+# 406,693,695 - ett extremvärde vid randen
+
+
