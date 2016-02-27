db28631936b02befb4668bd0f586d25b8c0e11ac
Tobychev <tobychev@live.com>
2016-02-27 13:23:07 +0100

    Refactored to use step plot wherever appropriate

--- visualize.py
+++ visualize.py
@@ -7,7 +7,7 @@ def get_colours(num,colour_name="Oranges"):
     return iter(cm.__dict__[colour_name](np.linspace(0,1,num)))
 
 def show_linegroup_on_curve(lines,xs,ys):
-    pl.plot(xs,ys)
+    pl.step(xs,ys)
     for line in lines:
         pl.plot(line.lmbd,line.ref,'ro-')
 
@@ -15,7 +15,7 @@ def show_linegroup_on_curve(lines,xs,ys):
 
 def show_autoline_on_curve(autoline,xs,ys):
     line = list(autoline)
-    pl.plot(xs,ys)
+    pl.step(xs,ys)
     pl.plot(xs[line],ys[line],'ro-')
     pl.show()
 
@@ -28,7 +28,7 @@ def show_spec_lines(spec,lines):
     ax  = fig.add_subplot(1,1,1)
     colour = get_colours(len(lines))
 
-    ax.plot(spec.lmbd,spec.ref)
+    ax.step(spec.lmbd,spec.ref)
     for i,line in enumerate(lines):
         ax = make_line_on_curve(line,spec.lmbd,spec.ref,ax,colour=next(colour))
 
@@ -36,7 +36,7 @@ def show_spec_lines(spec,lines):
 
 def show_dict_wins_on_curve(wins,xs,ys):
     colour = get_colours(len(wins))
-    pl.plot(xs,ys)
+    pl.step(xs,ys)
     for i,name in enumerate(wins):
         idx = wins[name]
         pl.plot(xs[idx],ys[idx],'o',c=next(colour))
@@ -48,14 +48,14 @@ def show_fit_with_points(spec,idx,fit,line="mean"):
         data = spec.mean
     else:
         data = spec.spec(line)
-    pl.plot(spec.lmbd,spec.mean)
+    pl.step(spec.lmbd,spec.mean)
     pl.plot(spec.lmbd[idx],spec.mean[idx],'ro')
     pl.plot(spec.lmbd[idx], fit[1]+ fit[0]*spec.lmbd[idx])
     pl.show()
 
 def show_fit_on_curve(fit,xs,curve):
     pl.plot(xs,fit[0]*xs+fit[1],'r')
-    pl.plot(xs,curve,'b')
+    pl.step(xs,curve,'b')
     pl.show()
 
 def plot_fits_stats(data,ref,names,title="",bins=43,cols=2,yscale="linear",cutoff=0):
@@ -99,7 +99,7 @@ def show_line_and_corefit(line,frame,row,width=3,fast=True):
     idfit = line.idx[fit < 1.05]
     fit   = fit[fit < 1.05]
 
-    pl.plot(lmd[line.idx],spe[line.idx],'b')
+    pl.step(lmd[line.idx],spe[line.idx],'b')
     pl.plot(lmd[bottom],spe[bottom],'*k')
     pl.plot(lmd[idfit],fit,'r')
 
