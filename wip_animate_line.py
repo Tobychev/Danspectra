3df1d156d2cf670f8d05c839a83baa78a9302c93
Tobychev <tobychev@live.com>
2016-03-10 00:07:59 +0100

    Changed to make animation for presenting.

--- wip_animate_line.py
+++ wip_animate_line.py
@@ -10,7 +10,7 @@ def linevel(lam):
     return c*(lam - line.cent)/line.cent
 
 lam  = frm.group.lmbd
-lbox3 = np.convolve(lam[line.idx],np.ones(17)/17.,"valid")
+lbox3 = np.convolve(lam[line.idx],np.ones(3)/3.,"valid")
 lbox3 = linevel(lbox3)
 
 lsav73 = sg.savgol_filter(lam[line.idx],7,3)
@@ -19,9 +19,9 @@ lsav73 = linevel(lsav73)
 
 fig = pl.figure()
 ax = pl.axes(
-#            xlim=(lbox3[-1], lbox3[0]),
+            xlim=(lbox3[-1], lbox3[0]),
 #            xlim=(lsav73[-1], lsav73[0]),
-            xlim=(lam[line.idx[-1]], lam[line.idx[0]]),
+#            xlim=(lam[line.idx[-1]], lam[line.idx[0]]),
             ylim=(0.914,1.03 ))
 #            ylim=(-0.1,.1 ))
 
@@ -43,21 +43,20 @@ def onClick(event):
 
 def animate(i):
     data = frm.data[i,line.idx]
-#    ys = np.convolve(data,np.ones(17)/17.,"valid"); xs = lbox3
-    spli = intr.splrep(np.flipud(lam[line.idx]) ,np.flipud(data),s=0.007)  #Spline interpolation, smoothing hand-chosen, 0.0055 good
-    ys = intr.splev(lam[line.idx],spli); xs = lam[line.idx] 
+    ys = np.convolve(data,np.ones(3)/3.,"valid"); xs = lbox3; method="rolling mean 3 points"
+#    spli = intr.splrep(np.flipud(lam[line.idx]) ,np.flipud(data),s=0.007)  #Spline interpolation, smoothing hand-chosen, 0.0055 good
+#    ys = intr.splev(lam[line.idx],spli); xs = lam[line.idx]; method="spline smoot s=0.007" 
 #    ys = sg.wiener(data,mysize=3) - intr.splev(lam[line.idx],spli); xs = lam[line.idx] 
-#    ys = sg.wiener(data,mysize=3); xs = lam[line.idx] 
-#    ys = sg.savgol_filter(data[i,line.idx],7,3); xs = lsav73
+#    ys = sg.wiener(data,mysize=3); xs = lam[line.idx]; method="Wiener filter 3 points" 
+#    ys = sg.savgol_filter(data[i,line.idx],7,3); xs = lsav73; method="Savgol filter p=7 n=3 "
 #    ys = data; xs = lam[line.idx] 
     curve.set_data(xs, ys)
-    text.set_text('Row {}'.format(i+1) )
-    print i
+    text.set_text('Row {}, method {} '.format(i+1,method) )
     return curve,text
 
 
 fig.canvas.mpl_connect('button_press_event', onClick)
 anim = animation.FuncAnimation(fig, animate, init_func=animinit,
-                    frames=798, interval=375, blit=True)
-#anim.save('frame_aS1_381.mp4', fps=3)
+                    frames=798, interval=325, blit=True)
+anim.save('frame_aS1_381_rollmean3.mp4', fps=3)
 pl.show()
