6e02b142973541871eba9423065a205882c88544
Tobychev <tobychev@live.com>
2016-03-15 15:38:18 +0100

    Merged kernel regression convenience function

--- stats.py
+++ stats.py
@@ -1,4 +1,6 @@
 import numpy as np
+import pyqt_fit as fitter
+import pyqt_fit.nonparam_regression as smooth
 
 def print_dict_stats_with_ref(data,ref,keys,title="",q=95):
     print("\n"+ title)
@@ -15,3 +17,13 @@ def rmse_at_zero_of_topq(data,q,ax=0):
     zero = np.zeros(data.shape)
     data = np.where(data > np.percentile(data,q=q),data,zero)
     return np.sqrt( np.mean(data**2,axis=ax) )
+
+def kern_reg(xs,ys,cutx=8,bins=63):
+    reg = smooth.NonParamRegression(xs,ys,
+                                method=fitter.npr_methods.SpatialAverage())
+    reg.fit()
+    count, evxs = np.histogram(xs,bins)
+    tmp, = np.where(count > cutx)
+    evxs = evxs[slice(tmp[0],tmp[-1])]
+    return evxs,reg.evaluate(evxs)
+
