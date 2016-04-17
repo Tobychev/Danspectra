10c8bd2e877436ec3bba81aeff41c69a2020d125
Tomas Bylund <tomas.bylund@gmail.com>
2016-04-18 01:51:48 +0200

    Working on error estimates

--- 6405.py
+++ 6405.py
@@ -4,6 +4,7 @@ import astropy.io.fits as fts
 import binner as bn
 import spectra as spc
 import numpy as np
+import errors as er
 
 s1 = spc.SpectraFactory("data/6405_aS1")
 s1.frame_row_cut([0,799])
@@ -11,8 +12,9 @@ s1.contrast_cut(50)
 s1.set_continua("segments")
 as1 = s1.make_spectra()
 
-s2 = spc.SpectraFactory("data/6405_aS2",framerows=774)
+s2 = spc.SpectraFactory("data/6405_aS2",framerows=774,framecols=1446)
 s2.frame_row_cut([0,743])
+s2.frame_col_cut([0])
 s2.contrast_cut(85)
 s2.set_continua("segments")
 as2 = s2.make_spectra()
@@ -26,7 +28,7 @@ bS1 = b1.make_spectra()
 
 # Sunspot measurement
 # Need to cut first column, its trash
-if True:
+if False:
     umbra    = 0.35
     wall     = (0.35,0.75)
     penumbra = (0.75,0.89)
@@ -36,4 +38,34 @@ if True:
     as2pn    = s2.make_spectra_subset(as2,rowsubset=((as2con > penumbra[0]) & (as2con < penumbra[1])),desc="Penumbra")
     as2qu    = s2.make_spectra_subset(as2,rowsubset=(as2con >= quiet),desc="Quiet sun")
     
-    pl.plot(as2um.lmbd[1:], as2qu[:,1:].mean(axis=0));pl.show()
+    pl.plot(as2um.lmbd, as2um[:,:].mean(axis=0));pl.show()
+
+if True:
+    # For spline measurement
+    bot,cnt,fwhm,as12,fw13,as13,fw23,as23,err,ew,cont = np.arange(0,11)
+
+    wH2O  = [289, 342];   cH2O  = 640.86681253796701
+    wFeI  = [376, 441];   cFeI  = 640.80263785584464
+    wSiFe = [467, 519];   cSiFe = 640.72889305314993
+    wMyst = [646, 722];   cMyst = 640.57558287431198 #Hand placed limits from mean(axis=0)
+    wCNq  = [1006,1047];  cCNq  = 640.31162454661717 #Hand placed limits from mean(axis=0)
+
+    Myst = spc.splineline(wMyst,cMyst,as1.meta)
+    FeI  = spc.splineline(wFeI ,cFeI ,as1.meta)
+    SiFe = spc.splineline(wSiFe,cSiFe,as1.meta)
+    H2O  = spc.splineline(wH2O ,cH2O ,as1.meta)
+
+    print("Measuring unknown line")
+    mesMyst = Myst.measure(as1)
+    print("Measuring Atmo H2O line")
+    mesH2O  = H2O.measure(as1) # Seems to depend on continuua, so probably not atmo line?
+    print("Measuring Iron line")
+    mesFeI  = FeI.measure(as1)
+    print("Measuring Si + Fe line")
+    mesSiFe = SiFe.measure(as1)
+    linmap = vis.spline_linemap(mesSiFe,SiFe)
+    err = np.load("Estimate_errSiFe.npy")
+    errSiFe = er.scale_spline_err(SiFe,mesSiFe,err)
+    vis.dan_errplot(linmap,errSiFe).show()
+#    pl.plot(mesMyst[:,cont],mesMyst[:,bot],'o')
+    
