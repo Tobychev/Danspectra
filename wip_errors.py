e860051838cb1befaca2dc70202dba668fb1ea89
Tomas Bylund <tomas.bylund@gmail.com>
2016-04-20 04:07:12 +0200

    Redone to estimate errors over a range of some parameter

--- wip_errors.py
+++ wip_errors.py
@@ -2,13 +2,17 @@ import matplotlib.pyplot as pl
 import scipy.stats as st
 import spectra as spc
 import numpy as np
+import errors as er
 import copy 
 
 from imp import reload
 
 reload(spc)
 
-vel = 0; bot = 1; cont = 2; err = 3; ew  = 4; mn  = 5; var = 6; ske = 7; kur = 8; wvar = 9; wske = 10; wkur = 11;
+# For moments measurement
+#vel,bot,cont,err,ew,mn,var,ske,kur,wvar,wske,wkur = np.arange(0,12)
+# For spline measurement
+bot,cnt,fwhm,as12,fw13,as13,fw23,as23,err,ew,cont = np.arange(0,11)
 
 wCN   = [289, 342];   cCN   = 640.86681253796701 
 wFeI  = [376, 441];   cFeI  = 640.80263785584464
@@ -22,40 +26,48 @@ sf.contrast_cut(50)
 sf.set_continua("segments")
 
 sp = sf.make_spectra()
-
-# Hand picked flat area
-wFlat = [1179,1239]; cFlat = sp.lmbd[range(wFlat[0],wFlat[1]+1)].mean()
-
-
-norm = st.norm.pdf(np.linspace(-6,6,60))
-
-simFeI  = 1 - 0.62*norm/norm.max() # Line values eye estimated from atlas
-simSiFe = 1 - 0.19*norm/norm.max() # Line values eye estimated from atlas
-simMyst = 1 - 0.09*norm/norm.max() # Line values eye estimated from atlas
-
-def mystmutant(x):
-    x[:,wFlat[0]:wFlat[1]] = x[:,wFlat[0]:wFlat[1]]*simSiFe
-    return x
-
-my = copy.deepcopy(sp) 
-my.modify(mystmutant)
-
-Flat = spc.splineline(wFlat,cFlat,my.meta)
-mesFlat =  Flat.measure(my)
-sigm = np.zeros((11,5))
-for i in range(0,11):
-    # The percentile ranges are one and two sigmas, calculated by hand from a probability table on wikipedia
-    sigm[i,:]= (
-        np.percentile(mesFlat[:,i],2.274),
-        np.percentile(mesFlat[:,i],15.87),
-        mesFlat[:,i].mean(),
-        np.percentile(mesFlat[:,i],84.13),
-        np.percentile(mesFlat[:,i],97.725))
-    m2s,m1s,mu,p1s,p2s = sigm[i,:]
-    print("-2s: {: 8.6f}, -1s {: 8.6f}, mu {: 8.6f}, +1s {: 8.6f}, +2s {: 8.6f}".format( m2s/mu,m1s/mu,mu,p1s/mu,p2s/mu ))
-
 def kde(idx):
     rt =  st.gaussian_kde(mesFlat[:,idx])
     x  = np.linspace(mesFlat[:,idx].min(),mesFlat[:,idx].max(),121)
     pl.plot(x,rt(x))
     pl.show()
+
+def mystmutant(x,dist):
+    x[:,wFlat[0]:wFlat[1]] = x[:,wFlat[0]:wFlat[1]]*dist
+    return x
+
+def percentiles(measure):
+    return ( np.percentile(measure,2.274),
+             np.percentile(measure,15.87),
+             measure.mean(),
+             np.percentile(measure,84.13),
+             np.percentile(measure,97.725))
+
+def intervalplot(errors,valrange,prop):
+    pl.plot(valrange,errors[:,prop,4],'r')
+    pl.plot(valrange,errors[:,prop,3],'b')
+    pl.plot(valrange,errors[:,prop,1],'b')
+    pl.plot(valrange,errors[:,prop,0],'r')
+    pl.show()
+
+
+# Hand picked flat area
+wFlat = [1179,1239]; cFlat = sp.lmbd[range(wFlat[0],wFlat[1]+1)].mean()
+
+# Range of line bottoms.
+botrange = np.linspace(0.25,0.9,11)
+varrange = np.linspace(0.6,2.5,14)
+
+errors = []
+bot = 0.8
+for var in varrange:
+    norm = st.norm.pdf(np.linspace(-6,6,60),scale=var); norm = norm/norm.max()
+    line = 1-bot*norm
+    my = copy.deepcopy(sp) 
+    my.modify(lambda x: mystmutant(x,line))
+    print("Estimating errors for {}".format(bot))
+    Flat = spc.splineline(wFlat,cFlat,my.meta)
+    mesFlat =  Flat.measure(my)
+    errors.append( er.err_spline_mes(mesFlat) )
+
+errors = np.array(errors)
