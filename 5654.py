7cadcd9baae2386ed4bb811ea60acca3efea524b
Tomas Bylund <tomas.bylund@gmail.com>
2016-04-23 20:05:52 +0200

    Added a few comments

--- 5654.py
+++ 5654.py
@@ -16,17 +16,17 @@ sf56_as2.frame_row_cut([0,755])
 sf56_as2.frame_col_cut([0])
 sf56_as2.contrast_cut(20)
 sf56_as2.set_continua("segments")
-as2 = sf56_as2.make_spectra()
-as2con = as2.meta.cont[0]*as2.lmbd.mean() + as2.meta.cont[1]
+as2 = sf56_as2.make_spectra() # Do this before defining lines
+as2con = as2.meta.cont[0]*as2.lmbd.mean() + as2.meta.cont[1] # Define continua for as2 series
 
-as2FeIp = spc.splineline(wFeIp, cFeIp, as2.meta,"FeIp ")
+as2FeIp = spc.splineline(wFeIp, cFeIp, as2.meta,"FeIp ") # Define lines for as2 series...
 as2Myst = spc.splineline(wMyst, cMyst, as2.meta,"Myst ")
 as2SiI  = spc.splineline(wSiI , cSiI , as2.meta,"SiI " )
 as2VI   = spc.splineline(wVI  , cVI  , as2.meta,"VI "  )
 as2ScII = spc.splineline(wScII, cScII, as2.meta,"ScII ")
 as2FeI  = spc.splineline(wFeIp, cFeI , as2.meta,"FeI " )
 
-as2lines = [as2FeIp,as2Myst,as2SiI,as2VI,as2ScII,as2FeI]
+as2lines = [as2FeIp,as2Myst,as2SiI,as2VI,as2ScII,as2FeI] # ... and save in this list for as2
 
 sf56_cs2 = spc.SpectraFactory("data/5654_cS2",framerows=756,framecols=1480)
 sf56_cs2.frame_row_cut([0,755])
@@ -34,16 +34,18 @@ sf56_cs2.frame_col_cut([0])
 sf56_cs2.contrast_cut(20)
 sf56_cs2.set_continua("segments")
 cs2 = sf56_cs2.make_spectra()
-cs2con = cs2.meta.cont[0]*cs2.lmbd.mean() + cs2.meta.cont[1]
+cs2con = cs2.meta.cont[0]*cs2.lmbd.mean() + cs2.meta.cont[1] # Define continua for cs2 series
 
-cs2FeIp = spc.splineline(wFeIp, cFeIp, cs2.meta,"FeIp ")
+
+cs2FeIp = spc.splineline(wFeIp, cFeIp, cs2.meta,"FeIp ") # Define lines for cs2 series...
 cs2Myst = spc.splineline(wMyst, cMyst, cs2.meta,"Myst ")
 cs2SiI  = spc.splineline(wSiI , cSiI , cs2.meta,"SiI " )
 cs2VI   = spc.splineline(wVI  , cVI  , cs2.meta,"VI "  )
 cs2ScII = spc.splineline(wScII, cScII, cs2.meta,"ScII ")
 cs2FeI  = spc.splineline(wFeIp, cFeI , cs2.meta,"FeI " )
 
-cs2lines = [cs2FeIp,cs2Myst,cs2SiI,cs2VI,cs2ScII,cs2FeI]
+cs2lines = [cs2FeIp,cs2Myst,cs2SiI,cs2VI,cs2ScII,cs2FeI] # ... and save in this list for cs2
+
 
 # Updating the limits to better match the meanspectra of this series
 wFeIp = [1217,1253]; cFeIp = 565.1477
@@ -59,16 +61,18 @@ sf56_bs2.frame_col_cut([0])
 sf56_bs2.contrast_cut(50)
 sf56_bs2.set_continua("segments")
 bs2 = sf56_bs2.make_spectra()
-bs2con = bs2.meta.cont[0]*bs2.lmbd.mean() + bs2.meta.cont[1]
+bs2con = bs2.meta.cont[0]*bs2.lmbd.mean() + bs2.meta.cont[1] # Define continua for bs2 series
 
-bs2FeIp = spc.splineline(wFeIp, cFeIp, bs2.meta,"FeIp ")
+bs2FeIp = spc.splineline(wFeIp, cFeIp, bs2.meta,"FeIp ") # Define lines for bs2 series...
+
 bs2Myst = spc.splineline(wMyst, cMyst, bs2.meta,"Myst ")
 bs2SiI  = spc.splineline(wSiI , cSiI , bs2.meta,"SiI " )
 bs2VI   = spc.splineline(wVI  , cVI  , bs2.meta,"VI "  )
 bs2ScII = spc.splineline(wScII, cScII, bs2.meta,"ScII ")
 bs2FeI  = spc.splineline(wFeIp, cFeI , bs2.meta,"FeI " )
 
-bs2lines = [bs2FeIp,bs2Myst,bs2SiI,bs2VI,bs2ScII,bs2FeI]
+bs2lines = [bs2FeIp,bs2Myst,bs2SiI,bs2VI,bs2ScII,bs2FeI] # ... and save in this list for bs2
+
 
 umbra    = 0.28
 wall     = (0.28,0.60)
