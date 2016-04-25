f7dab9c002bbd6062d7bf69335c1b26945e91d0e
Tomas Bylund <tomas.bylund@gmail.com>
2016-04-26 01:02:13 +0200

    Reordering code and renaming to export symbols consistently

--- 5654.py
+++ 5654.py
@@ -11,54 +11,53 @@ wVI   = [567,595];   cVI   = 565.7456
 wScII = [512,548];   cScII = 565.7880
 wFeI  = [131,168];   cFeI  = 566.1354
 
-lims = {}
-lims["ewlim"]   = ( 0.3 , 1.8  )
-lims["vellim"]  = (-5.8 , 6.1  )
-lims["rellim"]  = ( 0.2 , 1.5  )
-lims["fw13lim"] = ( -0.1 , 1.0  )
-lims["fwhmlim"] = ( -0.1 , 1.0  )
-lims["fw23lim"] = (-0.2 , 1.5  )
-lims["as13lim"] = (-0.2 , 0.2  )
-lims["as12lim"] = (-0.3 , 0.2  )
-lims["as23lim"] = (-0.7 , 0.5  )
-as2lims = lims
-
 sf56_as2 = spc.SpectraFactory("data/5654_aS2",framerows=756,framecols=1480)
 sf56_as2.frame_row_cut([0,755])
 sf56_as2.frame_col_cut([0])
 sf56_as2.contrast_cut(20)
 sf56_as2.set_continua("segments")
-as2 = sf56_as2.make_spectra() # Do this before defining lines
-as2con = as2.meta.cont[0]*as2.lmbd.mean() + as2.meta.cont[1] # Define continua for as2 series
 
-as2FeIp = spc.splineline(wFeIp, cFeIp, as2.meta,"FeIp ") # Define lines for as2 series...
-as2Myst = spc.splineline(wMyst, cMyst, as2.meta,"Myst ")
-as2SiI  = spc.splineline(wSiI , cSiI , as2.meta,"SiI " )
-as2VI   = spc.splineline(wVI  , cVI  , as2.meta,"VI "  )
-as2ScII = spc.splineline(wScII, cScII, as2.meta,"ScII ")
-as2FeI  = spc.splineline(wFeI, cFeI , as2.meta,"FeI " )
+qu1 = sf56_as2.make_spectra() # Do this before defining lines
+qu1con = qu1.meta.cont[0]*qu1.lmbd.mean() + qu1.meta.cont[1] # Define continua for qu1 series
 
-as2lines = [as2FeIp,as2Myst,as2SiI,as2VI,as2ScII,as2FeI] # ... and save in this list for as2
+qu1FeIp = spc.splineline(wFeIp, cFeIp, qu1.meta,"FeIp ") # Define lines for qu1 series...
+qu1Myst = spc.splineline(wMyst, cMyst, qu1.meta,"Myst ")
+qu1SiI  = spc.splineline(wSiI , cSiI , qu1.meta,"SiI " )
+qu1VI   = spc.splineline(wVI  , cVI  , qu1.meta,"VI "  )
+qu1ScII = spc.splineline(wScII, cScII, qu1.meta,"ScII ")
+qu1FeI  = spc.splineline(wFeI, cFeI , qu1.meta,"FeI "  )
+qu1lines = [qu1FeIp,qu1Myst,qu1SiI,qu1VI,qu1ScII,qu1FeI] # ... and save in this list for qu1
 
-cs2lims=lims
+lims = {}
+lims["ewlim"]   = ( 0.3 , 1.8 )
+lims["vellim"]  = (-5.8 , 6.1 )
+lims["rellim"]  = ( 0.2 , 1.5 )
+lims["fw13lim"] = (-0.1 , 1.0 )
+lims["fwhmlim"] = (-0.1 , 1.0 )
+lims["fw23lim"] = (-0.2 , 1.5 )
+lims["as13lim"] = (-0.2 , 0.2 )
+lims["as12lim"] = (-0.3 , 0.2 )
+lims["as23lim"] = (-0.7 , 0.5 )
+qu1lims = lims
+
 sf56_cs2 = spc.SpectraFactory("data/5654_cS2",framerows=756,framecols=1480)
 sf56_cs2.frame_row_cut([0,755])
 sf56_cs2.frame_col_cut([0])
 sf56_cs2.contrast_cut(20)
 sf56_cs2.set_continua("segments")
-cs2 = sf56_cs2.make_spectra()
-cs2con = cs2.meta.cont[0]*cs2.lmbd.mean() + cs2.meta.cont[1] # Define continua for cs2 series
 
+qu2 = sf56_cs2.make_spectra()
+qu2con = qu2.meta.cont[0]*qu2.lmbd.mean() + qu2.meta.cont[1] # Define continua for qu2 series
 
-cs2FeIp = spc.splineline(wFeIp, cFeIp, cs2.meta,"FeIp ") # Define lines for cs2 series...
-cs2Myst = spc.splineline(wMyst, cMyst, cs2.meta,"Myst ")
-cs2SiI  = spc.splineline(wSiI , cSiI , cs2.meta,"SiI " )
-cs2VI   = spc.splineline(wVI  , cVI  , cs2.meta,"VI "  )
-cs2ScII = spc.splineline(wScII, cScII, cs2.meta,"ScII ")
-cs2FeI  = spc.splineline(wFeI, cFeI , cs2.meta,"FeI " )
+qu2FeIp = spc.splineline(wFeIp, cFeIp, qu2.meta,"FeIp ") # Define lines for qu2 series...
+qu2Myst = spc.splineline(wMyst, cMyst, qu2.meta,"Myst ")
+qu2SiI  = spc.splineline(wSiI , cSiI , qu2.meta,"SiI " )
+qu2VI   = spc.splineline(wVI  , cVI  , qu2.meta,"VI "  )
+qu2ScII = spc.splineline(wScII, cScII, qu2.meta,"ScII ")
+qu2FeI  = spc.splineline(wFeI, cFeI , qu2.meta,"FeI "  )
+qu2lines = [qu2FeIp,qu2Myst,qu2SiI,qu2VI,qu2ScII,qu2FeI] # ... and save in this list for qu2
 
-cs2lines = [cs2FeIp,cs2Myst,cs2SiI,cs2VI,cs2ScII,cs2FeI] # ... and save in this list for cs2
-
+qu2lims=lims
 
 # Updating the limits to better match the meanspectra of this series
 wFeIp = [1217,1253]; cFeIp = 565.1477
@@ -68,34 +67,33 @@ wVI   = [564,596];   cVI   = 565.7456
 wScII = [512,555];   cScII = 565.7880
 wFeI  = [139,167];   cFeI  = 566.1354
 
-lims["ewlim"]   = ( 0 , 5  )
-lims["vellim"]  = (-5.8 , 7  )
-lims["rellim"]  = ( 0 , 6  )
-lims["fw13lim"] = ( -0.1 , 1.0  )
-lims["fwhmlim"] = ( -0.1 , 1.1  )
-lims["fw23lim"] = (-0.2 , 1.5  )
-lims["as13lim"] = (-0.4 , 0.3  )
-lims["as12lim"] = (-0.5 , 0.4  )
-lims["as23lim"] = (-0.9 , 0.5  )
-bs2lims = lims
 
 sf56_bs2 = spc.SpectraFactory("data/5654_bS2",framerows=756,framecols=1480)
 sf56_bs2.frame_row_cut([0,755])
 sf56_bs2.frame_col_cut([0])
 sf56_bs2.contrast_cut(50)
 sf56_bs2.set_continua("segments")
-bs2 = sf56_bs2.make_spectra()
-bs2con = bs2.meta.cont[0]*bs2.lmbd.mean() + bs2.meta.cont[1] # Define continua for bs2 series
+spt = sf56_bs2.make_spectra()
+sptcon = spt.meta.cont[0]*spt.lmbd.mean() + spt.meta.cont[1] # Define continua for spt series
 
-bs2FeIp = spc.splineline(wFeIp, cFeIp, bs2.meta,"FeIp ") # Define lines for bs2 series...
-bs2Myst = spc.splineline(wMyst, cMyst, bs2.meta,"Myst ")
-bs2SiI  = spc.splineline(wSiI , cSiI , bs2.meta,"SiI " )
-bs2VI   = spc.splineline(wVI  , cVI  , bs2.meta,"VI "  )
-bs2ScII = spc.splineline(wScII, cScII, bs2.meta,"ScII ")
-bs2FeI  = spc.splineline(wFeI, cFeI , bs2.meta,"FeI " )
+sptFeIp = spc.splineline(wFeIp, cFeIp, spt.meta,"FeIp ") # Define lines for spt series...
+sptMyst = spc.splineline(wMyst, cMyst, spt.meta,"Myst ")
+sptSiI  = spc.splineline(wSiI , cSiI , spt.meta,"SiI " )
+sptVI   = spc.splineline(wVI  , cVI  , spt.meta,"VI "  )
+sptScII = spc.splineline(wScII, cScII, spt.meta,"ScII ")
+sptFeI  = spc.splineline(wFeI, cFeI , spt.meta,"FeI "  )
+sptlines = [sptFeIp,sptMyst,sptSiI,sptVI,sptScII,sptFeI] # ... and save in this list for spt
 
-bs2lines = [bs2FeIp,bs2Myst,bs2SiI,bs2VI,bs2ScII,bs2FeI] # ... and save in this list for bs2
-
+lims["ewlim"]   = ( 0   , 5   )
+lims["vellim"]  = (-5.8 , 7   )
+lims["rellim"]  = ( 0   , 6   )
+lims["fw13lim"] = (-0.1 , 1.0 )
+lims["fwhmlim"] = (-0.1 , 1.1 )
+lims["fw23lim"] = (-0.2 , 1.5 )
+lims["as13lim"] = (-0.4 , 0.3 )
+lims["as12lim"] = (-0.5 , 0.4 )
+lims["as23lim"] = (-0.9 , 0.5 )
+sptlims = lims
 
 umbra    = 0.28
 wall     = (0.28,0.60)
