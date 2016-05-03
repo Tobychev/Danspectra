9745d1c571344ef2b9c23a234b8b4089cdd6ff11
Tomas Bylund <tomas.bylund@gmail.com>
2016-05-03 14:04:47 +0200

    Merge in changes from wip, and put in try except guards

--- measure.py
+++ measure.py
@@ -1,28 +1,44 @@
-import matplotlib.pyplot as pl
-import visualize as vis
 import numpy as np
 
-regname = "6405"
-typname = "spt"
+regnames = ["5053","5215","5654","6405","6449"]
 
-region = __import__(regname)
+for regname in regnames:
+    region = __import__(regname)
 
-spec  = region.spt
-lines = region.sptlines
-lims  = region.sptlims
+    spec1  = region.qu1
+    lines1 = region.qu1lines
+    res = {} 
+    for i,line in enumerate(lines1):
+        name ="{}_{}".format(line.name.split()[0],int(line.cent*10))
+        try:
+            res[name] = line.measure(spec1)
+        except Exception as err:
+            print("Line {} failed to measure:".format(name))
+            print(err)
+    np.savez_compressed("bin/{}_qu1".format(regname),**res)
 
-res = {} 
-for i,line in enumerate(lines):
-    name = line.name.split(" ")[0]
-    res[name] = line.measure(spec)
+    spec2  = region.qu2
+    lines2 = region.qu2lines
+    res = {} 
+    for i,line in enumerate(lines2):
+        name ="{}_{}".format(line.name.split()[0],int(line.cent*10))
+        try:
+            res[name] = line.measure(spec2)
+        except Exception as err:
+            print("Line {} failed to measure:".format(name))
+            print(err)
+    np.savez_compressed("bin/{}_qu2".format(regname),**res)
 
-np.savez("{}-{}".format(regname,typname),res)
-
-for i,line in enumerate(lines):
-    name = line.name.split(" ")[0]
-    fig = vis.spline_linemap(res[name],lines[i],lims=lims)
-    fig.set_size_inches([ 14.2625,   10.])
-    fig.savefig("../thesis/figures/{}_{}_{}.png".format(regname,typname,name))
-    pl.show(block=False)
+    spec3  = region.spt
+    lines3 = region.sptlines
+    res = {} 
+    for i,line in enumerate(lines3):
+        name ="{}_{}".format(line.name.split()[0],int(line.cent*10))
+        try:
+            res[name] = line.measure(spec3)
+        except Exception as err:
+            print("Line {} failed to measure:".format(name))
+            print(err)
+    np.savez_compressed("bin/{}_spt".format(regname),**res)
 
 
