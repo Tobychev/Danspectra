ded89772c13c9a42bc51382d5f38dfc91e1a4df6
Tomas Bylund <tomas.bylund@gmail.com>
2016-05-06 04:08:26 +0200

    Updated to load lines from pickles, and plot using new line names

--- il_make_linemaps.py
+++ il_make_linemaps.py
@@ -1,15 +1,18 @@
+import matplotlib.pyplot as pl
 import visualize as vis
 import pickle as pic
 import numpy as np
 
-dat = np.load("bin/5053_qu1.npz")
-lines=[lin[key] for key in lin.keys()]
+
+dat = np.load("bin/6449_qu2.npz")
+lines = pic.load(open("bin/6449_qu2.lin","rb"))
 lims = vis.make_linemap_lims([dat[key] for key in dat.keys()])
 
+pl.close("all")
 
 for i,line in enumerate(lines):
-    name = line.name.split(" ")[0]
-    fig = vis.spline_linemap(res[name],lines[i],lims=lims)
+    name ="{}_{}".format(line.name.split()[0],int(line.cent*10))
+    fig = vis.spline_linemap(dat[name],lines[i],lims=lims)
     fig.set_size_inches([ 14.2625,   10.])
 #   fig.savefig("../thesis/figures/{}_{}_{}.png".format(regname,typname,name))
     pl.show(block=False)
