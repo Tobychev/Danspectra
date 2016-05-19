364f088603c417228313dcb3cce5da90ccc5ac9c
Tomas Bylund <tomas.bylund@gmail.com>
2016-05-20 00:35:05 +0200

    Fixed so all spotspectra get made, and correctly named

--- illustrations/il_spotspectra.py
+++ illustrations/il_spotspectra.py
@@ -5,28 +5,36 @@ import visualize as vis
 import numpy as np
 
 regname = "6449"
-region = __import__(regname)
-spt = region.spt
-con = region.sptcon
-um, = np.where(con < region.um)
-wl, = np.where((region.um <= con ) & (con < region.wl))
-pn, = np.where((region.wl <= con ) & (con < region.pn))
-qs, = np.where(region.pn <= con)
 
-colr = cm.bwr
-fig,ax = pl.subplots(1)
-ax.plot(np.ones(2)*region.sptMyst.cent,[1.01,1.03],'k',lw=1)
-ax.plot(spt.lmbd,spt[um,:].mean(axis=0),color=colr(0.15),label="Umbra")
-ax.plot(spt.lmbd,spt[wl,:].mean(axis=0),color=colr(0.4), label="Wall")
-ax.plot(spt.lmbd,spt[pn,:].mean(axis=0),color=colr(0.6), label="Penumbra")
-ax.plot(spt.lmbd,spt[qs,:].mean(axis=0),color=colr(0.75),label="Quiet sun")
+def spotspec(regname):
 
-ax.set_xlabel("Wavelenght [nm]")
-ax.set_ylabel("Relative intensity")
-ax.set_xlim(region.xspotlims); 
-ax.set_ylim(region.yspotlims); 
-ax.xaxis.set_major_formatter(tck.StrMethodFormatter("{x:6.2f}"))
-pl.legend(loc="lower left")
-fig.savefig("../thesis/figures/spot6449.png")
-fig.show()
+    region = __import__(regname)
+    spt = region.spt
+    con = region.sptcon
+    um, = np.where(con < region.um)
+    wl, = np.where((region.um <= con ) & (con < region.wl))
+    pn, = np.where((region.wl <= con ) & (con < region.pn))
+    qs, = np.where(region.pn <= con)
 
+    colr = cm.bwr
+    fig,ax = pl.subplots(1)
+    ax.plot(np.ones(2)*region.sptMyst.cent,[1.01,1.03],'k',lw=1)
+    ax.plot(spt.lmbd,spt[um,:].mean(axis=0),color=colr(0.15),label="Umbra")
+    ax.plot(spt.lmbd,spt[wl,:].mean(axis=0),color=colr(0.4), label="Wall")
+    ax.plot(spt.lmbd,spt[pn,:].mean(axis=0),color=colr(0.6), label="Penumbra")
+    ax.plot(spt.lmbd,spt[qs,:].mean(axis=0),color=colr(0.75),label="Quiet sun")
+
+    ax.set_xlabel("Wavelenght [nm]")
+    ax.set_ylabel("Relative intensity")
+    ax.set_xlim(region.xspotlims); 
+    ax.set_ylim(region.yspotlims); 
+    ax.xaxis.set_major_formatter(tck.StrMethodFormatter("{x:6.2f}"))
+    pl.legend(loc="lower left")
+#    fig.savefig("../thesis/figures/spot{}.png".format(regname))
+    fig.show()
+
+spotspec("5053")
+spotspec("5215")
+spotspec("5654")
+spotspec("6405")
+spotspec("6449")
