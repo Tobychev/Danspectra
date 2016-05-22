92ea78af339744c65552c82563782578118b8245
Tobychev <tobychev@live.com>
2016-05-22 04:01:25 +0200

    Finished making script for comparing various fts atlases

--- il_atlasref.py
+++ il_atlasref.py
@@ -6,24 +6,34 @@ import os
 
 def atlasplot(atlas,regname):
     reg = {"5053":19750,"5215":19150,"5654":17650,"6405":15600,"6449":15500}
+    xlim = {"5053":(505.39283866772666, 505.57376665634223),
+            "5215":(521.54930858395733, 521.81535307328977),
+            "5645":(565.36080224893726, 565.92956831584172),
+            "6405":(640.56549492715919, 640.8788013787721 ),
+            "6449":(644.92785094602743, 645.34074495254845)}
+    ylim = {"5053":(0.63975783972631528, 1.0820081717200596),
+            "5215":(0.63457553733797023, 1.1250485137224484),
+            "5645":(0.75923171274393061, 1.0552637808945176),
+            "6405":(0.86458333333333337, 1.0437500000000002),
+            "6449":(0.67393994279930325, 1.0507659350912264)}
     qs = atlas["qs{}".format(reg[regname])]
     sp = atlas["sp{}".format(reg[regname])]
     lm = atlas["lm{}".format(reg[regname])]
-    region = __import__(regname)
+#    region = __import__(regname)
     
-    colr = cmm.Oranges
+    colr = cmm.RdYlGn
     fig,ax = pl.subplots(1)
-    ax.plot(1e7/qs[::-1,0],qs[::-1,1],color=colr(0.75),label="Quiet sun")
-    ax.plot(lm[:,0]/10,lm[:,1],color=colr(0.55), label="Limb")
-    ax.plot(1e7/sp[::-1,0],sp[::-1,1],color=colr(0.15),label="Spot")
+    ax.plot(1e7/qs[::-1,0],qs[::-1,1],color=colr(0.95),label="Quiet sun")
+    ax.plot(lm[:,0]/10,lm[:,1],color=colr(0.70), label="Limb")
+    ax.plot(1e7/sp[::-1,0],sp[::-1,1],color=colr(0.20),label="Spot")
 
     ax.set_xlabel("Wavelenght [nm]")
     ax.set_ylabel("Relative intensity")
-    ax.set_xlim(region.xspotlims); 
-    ax.set_ylim(region.yspotlims); 
+    ax.set_xlim(xlim[regname]); 
+    ax.set_ylim(ylim[regname]); 
     ax.xaxis.set_major_formatter(tck.StrMethodFormatter("{x:6.2f}"))
     pl.legend(loc="lower left")
-#    fig.savefig("../thesis/figures/spot{}.png".format(regname))
+    fig.savefig("../thesis/figures/atlascmp{}.png".format(regname))
     fig.show()
 
 
@@ -37,7 +47,7 @@ quietatlas = list(filter(lambda x: x[-2:]=="qs",
 quietatlas.sort()
 
 limbatlas = np.loadtxt(datadir+"FTS_atlas_limb.txt",skiprows=2)[:,[0,2]]
-limblines = {"5053":[19750,193091,195955,0.1394],"5215":[19150,224647,227693,0.1435],"5654":[17650,312894,316479,0.1548],"6405":[15600,460851,465439,0.1776],"6449":[15500,469066,473713,0.177]}
+limblines = {"5053":[19750,193091,195955,1.394],"5215":[19150,224647,227693,1.435],"5654":[17650,312894,316479,1.548],"6405":[15600,460851,465439,1.776],"6449":[15500,469066,473713,1.77]}
 
 
 atlas = {}
@@ -57,3 +67,8 @@ for key in limblines.keys():
     tmp[:,0] = tmp[:,0]+cor
     atlas["lm"+str(cm)] = tmp
 
+atlasplot(atlas,"5053")
+atlasplot(atlas,"5215")
+atlasplot(atlas,"5654")
+atlasplot(atlas,"6405")
+atlasplot(atlas,"6449")
