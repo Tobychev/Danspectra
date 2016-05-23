59ba27c7aa47144b04a1210d142eb6736fbe6e2e
Tomas Bylund <tomas.bylund@gmail.com>
2016-05-23 17:10:02 +0200

    Updated linetabs to state limits of wavelenght regions

--- illustrations/il_linetabs.py
+++ illustrations/il_linetabs.py
@@ -5,19 +5,25 @@ tabhead="""
 \centering
     \\begin{tabular}{lccccl}
 Species & $\lambda$  & E${}_l$ & $\log gf$ & depth & Comment\\\\ \hline"""
-tabline = "{}    & {:7.3f}    & {:5.3f}   & {:6.3f}    & {:5.3f} & \\\\"
+tabline = "{:<10}  & {:7.4f}   & {} & {}  & {:5.3f} & \\\\"
 tabfoot="""\
     \end{{tabular}}
-    \caption{{Lines in the {reg} region.}}\label{{tab:{reg}}}
+    \caption = "\caption{{Lines in the {reg} region, that extends from {start:.2f} to {finish:.2f} nm.}}\label{{tab:{reg}}}"
 \end{{table}}"""
 
 regions =["5053","5215","5654","6405","6449"]
 
 for region in regions:
+    reg = __import__(region)
     lfil = open("bin/{}_qu1.lin".format(region),"rb")
     lines = pic.load(lfil); lfil.close()
     print(tabhead)
     for line in lines:
-        print(tabline.format(line.name[:-7],line.cent,line.El,line.gf,line.dept))
-    print(tabfoot.format(reg=region))
+        if line.El < 0:
+            el = gf = "  -   "
+        else:
+            el = "{:5.3f} ".format(line.El)
+            gf = "{:6.3f}".format(line.gf)
+        print(tabline.format(line.name[:-7],line.cent,el,gf,line.dept))
+    print(tabfoot.format(reg=region,start=reg.qu1.lmbd[-1],finish=reg.qu1.lmbd[0]))
 
