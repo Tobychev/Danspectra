8c2ef61cd8a2c91187ee5e0147f66c82d5f8246c
Tomas Bylund <tomas.bylund@gmail.com>
2016-05-24 00:22:22 +0200

    Fixed captions and line depth

--- illustrations/il_linetabs.py
+++ illustrations/il_linetabs.py
@@ -8,7 +8,7 @@ Species & $\lambda$  & E${}_l$ & $\log gf$ & depth & Comment\\\\ \hline"""
 tabline = "{:<10}  & {:7.4f}   & {} & {}  & {:5.3f} & \\\\"
 tabfoot="""\
     \end{{tabular}}
-    \caption = "\caption{{Lines in the {reg} region, that extends from {start:.2f} to {finish:.2f} nm.}}\label{{tab:{reg}}}"
+    \caption{{Lines in the {reg} region, that extends from {start:.2f} to {finish:.2f} nm.}}\label{{tab:{reg}}}
 \end{{table}}"""
 
 regions =["5053","5215","5654","6405","6449"]
@@ -24,6 +24,6 @@ for region in regions:
         else:
             el = "{:5.3f} ".format(line.El)
             gf = "{:6.3f}".format(line.gf)
-        print(tabline.format(line.name[:-7],line.cent,el,gf,line.dept))
+        print(tabline.format(line.name[:-7],line.cent,el,gf,1-line.dept))
     print(tabfoot.format(reg=region,start=reg.qu1.lmbd[-1],finish=reg.qu1.lmbd[0]))
 
