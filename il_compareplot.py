08a637bdced5bf1f09c97cd50b77b115f0f8bd33
Tobychev <tobychev@live.com>
2016-06-09 13:03:36 +0200

    Added errors to the compareplot

--- il_compareplot.py
+++ il_compareplot.py
@@ -7,12 +7,12 @@ import numpy as np
 
 bot  = 0; vel  = 1; fwhm = 2; as12 = 3; fw13 = 4; as13 = 5; fw23 = 6; as23 = 7; err  = 8; ew   = 9; con  = 10;
 
-def comp_plot(datlist,xpos,ypos,normalize=True):
+def comp_plot(datlist,xpos,ypos,normalize=True,epos=None):
     cols = 4
     rows = int(np.ceil(len(datlist)/cols))
     fig,ax = pl.subplots(rows,cols,sharex=True,sharey=True)
 
-    for i,(x,y,name,_) in enumerate(datlist):
+    for i,(x,y,name,_,errs) in enumerate(datlist):
         if normalize:
             norm = y.mean()
             fig.axes[i].text(xpos,ypos,"{} {:5.1f}".format(name,norm))
@@ -20,10 +20,18 @@ def comp_plot(datlist,xpos,ypos,normalize=True):
             norm = 1
             fig.axes[i].text(xpos,ypos,"{}".format(name))
 
+        s2err = np.array([errs[3],errs[0]]).reshape(2,1)
+        s1err = np.array([errs[2],errs[1]]).reshape(2,1)
+
         regx,regy = st.kern_reg(x,y,bins=73)
         fig.axes[i].plot(x,y/norm,'b.',alpha=0.05)
         fig.axes[i].plot(regx,regy/norm,'w',linewidth=2.1)
         fig.axes[i].plot(regx,regy/norm,'r',linewidth=1.2)
+        if epos is not None:
+            ex,ey = epos
+            fig.axes[i].errorbar( ex,ey,yerr=s1err/norm,fmt='b',linewidth=1.5 )
+            fig.axes[i].errorbar( ex,ey,yerr=s2err/norm,fmt='r',alpha=0.3 )
+
 
     fig.axes[i].set_xlim(0.8,1.27)
     xtic,ytic = fig.axes[i].get_xticks(),fig.axes[i].get_yticks()
@@ -36,6 +44,8 @@ def comp_plot(datlist,xpos,ypos,normalize=True):
 regnames = ["5053","5215","5654","6405","6449"]
 
 errs = np.load("bin/errlimfile.npz")
+errs = {key: errs[key]  for key in errs.keys()}
+errs['Co_6454'] = errs['Co_6450'] # Quick fix, here to stay
 ewdata = []
 lcdata = []
 lddata = []
@@ -54,15 +64,16 @@ for region in regnames:
     lines = dict(map(dictgen,range(len(lines))))
 
     for line in dat.keys():
-        ewdata.append((dat[line][:,con],dat[line][:,ew],line,lines[line]))
-        lcdata.append((dat[line][:,con],dat[line][:,vel],line,lines[line]))
-        lddata.append((dat[line][:,con],dat[line][:,bot],line,lines[line]))
-        fwdata.append((dat[line][:,con],dat[line][:,fw13],line,lines[line]))
-        asdata.append((dat[line][:,con],dat[line][:,as13],line,lines[line]))
+        errs[line]
+        ewdata.append((dat[line][:,con],dat[line][:,ew]  ,line,lines[line],errs[line][ew,:]))
+        lcdata.append((dat[line][:,con],dat[line][:,vel] ,line,lines[line],errs[line][vel,:]))
+        lddata.append((dat[line][:,con],dat[line][:,bot] ,line,lines[line],errs[line][bot,:]))
+        fwdata.append((dat[line][:,con],dat[line][:,fw13],line,lines[line],errs[line][fw13,:]))
+        asdata.append((dat[line][:,con],dat[line][:,as13],line,lines[line],errs[line][as13,:]))
 
         
 if False:
-    fig = comp_plot(sorted(ewdata,key=lambda itm: 1-itm[3].dept,),0.83,1.06)
+    fig = comp_plot(sorted(ewdata,key=lambda itm: 1-itm[3].dept,),0.83,1.06,epos=(1.23,1.))
     fig.set_size_inches([ 8.8, 10.125])
     fig.tight_layout(h_pad=0.0, w_pad=0.0)
     fig.axes[-1].set_ylim(0.85248256275956014, 1.1380523623353493)
@@ -70,31 +81,32 @@ if False:
     fig.show()
 
 if False:
-    fig = comp_plot(sorted(lcdata,key=lambda itm: 1-itm[3].dept),0.83,2.6,normalize=False)
+    fig = comp_plot(sorted(lcdata,key=lambda itm: 1-itm[3].dept),0.83,2.6,normalize=False,epos=(1.23,.0))
     fig.set_size_inches([  8.8, 10.125 ])
     fig.tight_layout(h_pad=0.0, w_pad=0.0)
     fig.axes[-1].set_ylim(-1.8771761049602738, 4.2387981400066064)
-    pl.savefig("../thesis/figures/Centcompareplot.png")
+#    pl.savefig("../thesis/figures/Centcompareplot.png")
     fig.show()
 
 if False:
-    fig = comp_plot(sorted(lddata,key=lambda itm: 1-itm[3].dept),0.83,.975,normalize=True)
+    fig = comp_plot(sorted(lddata,key=lambda itm: 1-itm[3].dept),0.83,.975,normalize=True,epos=(1.23,1.))
     fig.set_size_inches([  8.8, 10.125 ])
     fig.tight_layout(h_pad=0.0, w_pad=0.0)
     fig.axes[-1].set_ylim(0.97007367457031757, 1.0252222422251558)
-    pl.savefig("../thesis/figures/Depthcompareplot.png")
-    fig.show()
-
-if True:
-    fig = comp_plot(sorted(asdata,key=lambda itm: 1-itm[3].dept),0.83,0.0007,normalize=False)
-    fig.set_size_inches([  8.8, 10.125 ])
-    fig.tight_layout(h_pad=0.0, w_pad=0.0)
-    fig.axes[-1].set_ylim(-0.0025418394542102273, 0.0018153436601899033)
-    pl.savefig("../thesis/figures/Asymmcompareplot.png")
+#    pl.savefig("../thesis/figures/Depthcompareplot.png")
     fig.show()
 
 if False:
-    fig = comp_plot(sorted(fwdata,key=lambda itm: 1-itm[3].dept),0.83,1.13,normalize=True)
+
+    fig = comp_plot(sorted(asdata,key=lambda itm: 1-itm[3].dept),0.83,0.0007,normalize=False,epos=(1.23,.0))
+    fig.set_size_inches([  8.8, 10.125 ])
+    fig.tight_layout(h_pad=0.0, w_pad=0.0)
+    fig.axes[-1].set_ylim(-0.0025418394542102273, 0.0018153436601899033)
+#    pl.savefig("../thesis/figures/Asymmcompareplot.png")
+    fig.show()
+
+if True:
+    fig = comp_plot(sorted(fwdata,key=lambda itm: 1-itm[3].dept),0.83,1.13,normalize=True,epos=(1.23,1.))
     fig.set_size_inches([  8.8, 10.125 ])
     fig.tight_layout(h_pad=0.0, w_pad=0.0)
     fig.axes[-1].set_ylim(0.78702842776826376, 1.2734822961034631)
