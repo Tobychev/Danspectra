6bb174d826dc37b9c8c8e030c290ddbc66aa0e21
Tomas Bylund <tomas.bylund@gmail.com>
2016-06-12 15:34:03 +0200

    Continuum def plots

--- il_present.py
+++ il_present.py
@@ -29,3 +29,21 @@ if False:
     ax.set_ylabel("Relative intensity")
     fg.savefig("../thesis/presentation/noise_spectra.png")
     fg.show()
+
+d =  __import__("5053")
+rndrow = 177
+con =  spc.continua(d.qu1m,d.qu1.lmbd,"top N")
+scon =  spc.continua(d.qu1m,d.qu1.lmbd,"segments")
+
+pl.plot(d.qu1.lmbd,d.qu1m)
+pl.plot(d.qu1.lmbd[con.idx],d.qu1m[con.idx],'rx')
+pl.plot(d.qu1.lmbd[scon.idx],d.qu1m[scon.idx],'ko')
+pl.show()
+
+n_con =  spc.continua(d.qu1[rndrow,:],d.qu1.lmbd,"top N")
+n_scon =  spc.continua(d.qu1[rndrow,:],d.qu1.lmbd,"segments")
+
+pl.plot(d.qu1.lmbd,d.qu1[rndrow,:])
+pl.plot(d.qu1.lmbd[n_con.idx],d.qu1m[n_con.idx],'rx')
+pl.plot(d.qu1.lmbd[n_scon.idx],d.qu1m[n_scon.idx],'ko')
+pl.show()
